34c31af7e9cf5485cc56bf4a46ca775c6f094db9
Silvia <santi.toledo1974@gmail.com>
2020-11-14 13:04:32 -0300

    up

---
+++ Clase 9/ejercicio2/index.js
@@ -0,0 +1,29 @@
+// npm install --save express nunjucks node-fetch
+const express = require('express');
+const nunjucks = require('nunjucks');
+const fetch = require('node-fetch');
+const app = express();
+app.use(express.static('public'));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', function(req, res) {
+  fetch('https://raw.githubusercontent.com/algolia/datasets/master/movies/actors.json')
+  .then(response => response.json())
+  .then(actores => res.render(
+  'index.html',{titulo:"Actores",t1:"Los actores",actores:actores,}))    
+});
+
+
+app.get('/actor/:id', function(req, res) {
+    fetch('https://raw.githubusercontent.com/algolia/datasets/master/movies/actors.json')
+    .then(response => response.json())
+    .then(actores => res.render(
+    'actor.html',{T2:"Actor ",actores:actores, id:req.params.id}))    
+  });
+
+
+app.listen(8080);
